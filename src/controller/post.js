f2fda930615f2562616a3052ca87843f1451d4dc
edig <edig@yad2.co.il>
2020-08-07 22:25:51 +0300

    save

--- src/controller/post.js
+++ src/controller/post.js
@@ -1,4 +1,4 @@
-const postService = require('../service/post')
+const postService = require('../src/service/post')
 
 async function getAllPosts(req, res) {
     const posts = await postService.getAllPosts()
